670ad82f0ce3b4087386f73e3731c9b32ab55a42
Shohei Aoki <shoaok@gmail.com>
2023-01-22 17:33:23 +0300

    use react for navbar

--- assets/src/app.tsx
+++ assets/src/app.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Teams from './components/teams'
+import App from './components/navbar'
  
 const teamDOM = document.getElementById('team')
 if (teamDOM !== null) render(<Teams />, teamDOM)
 
- 
+const navbarDOM = document.getElementById('navbar')
+if (navbarDOM !== null) render(<App />, navbarDOM)
