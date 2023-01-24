850620aece4743b8838d86fd3c33179998a23d54
Shohei Aoki <shoaok@gmail.com>
2023-01-24 04:58:08 +0300

    use react for footer

--- assets/src/app.tsx
+++ assets/src/app.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import Teams from './components/teams'
 import App from './components/navbar'
+import Footer from './components/footer'
  
 const teamDOM = document.getElementById('team')
 if (teamDOM !== null) render(<Teams />, teamDOM)
 
 const navbarDOM = document.getElementById('navbar')
 if (navbarDOM !== null) render(<App />, navbarDOM)
+
+const footerDOM = document.getElementById('footer')
+if (footerDOM !== null) render(<Footer />, footerDOM)
