f233434f2443efd6c97da23c7d18e1d66b04de0f
Shohei Aoki <shoaok@gmail.com>
2023-10-09 18:25:27 +0300

    update

---
+++ assets/src/components/analytics.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Analytics = () => {
+    return (
+<div>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1BMW6VHETF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1BMW6VHETF');
+</script>
+</div>
+    );
+  };
+  
+  export default Analytics;
