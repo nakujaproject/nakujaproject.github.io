9e66aee2901f91dea2009071ea9cd19485b25d3a
Shohei Aoki <shoaok@gmail.com>
2024-02-05 13:16:56 +0300

    add margin on navbar

--- assets/src/components/navbar.tsx
+++ assets/src/components/navbar.tsx
@@ -4,49 +4,55 @@ import { Navbar, Nav } from "react-bootstrap";
 class App extends Component {
   render() {
     return (
-      <Navbar className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Nav.Link className="navbar-item" href="/">
-            <img src="image/nakuja_logo.webp" height="28" />
-          </Nav.Link>
+      <section className="container">
+        <Navbar
+          className="navbar"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="navbar-brand">
+            <Nav.Link className="navbar-item" href="/">
+              <img src="image/nakuja_logo.webp" height="28" />
+            </Nav.Link>
 
-          <Nav.Link
-            role="button"
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </Nav.Link>
-        </div>
-        <Nav id="navbarBasicExample" className="navbar-menu navbar-end">
-          <Nav.Link className="navbar-item" href="about.html">
-            About 
-          </Nav.Link>
-          <Nav.Link className="navbar-item" href="team.html">
-            Team
-          </Nav.Link>
-          <Nav.Link className="navbar-item" href="rocket.html">
-            Rocket
-          </Nav.Link>
-          <Nav.Link className="navbar-item" href="research.html">
-            Research
-          </Nav.Link>
-          <Nav.Link className="navbar-item" href="join.html">
-            Join
-          </Nav.Link>
-          <Nav.Link
-            className="navbar-item"
-            href="https://nakujaproject.blogspot.com/"
-            target="_blank"
-          >
-            Blog
-          </Nav.Link>
-        </Nav>
-      </Navbar>
+            <Nav.Link
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </Nav.Link>
+          </div>
+          <Nav id="navbarBasicExample" className="navbar-menu navbar-end">
+            <Nav.Link className="navbar-item" href="about.html">
+              About
+            </Nav.Link>
+            <Nav.Link className="navbar-item" href="team.html">
+              Team
+            </Nav.Link>
+            <Nav.Link className="navbar-item" href="rocket.html">
+              Rocket
+            </Nav.Link>
+            <Nav.Link className="navbar-item" href="research.html">
+              Research
+            </Nav.Link>
+            <Nav.Link className="navbar-item" href="join.html">
+              Join
+            </Nav.Link>
+            <Nav.Link
+              className="navbar-item"
+              href="https://nakujaproject.blogspot.com/"
+              target="_blank"
+            >
+              Blog
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+      </section>
     );
   }
 }
